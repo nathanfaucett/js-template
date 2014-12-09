5550378eb6723f4f851c773c964e74dd4695d353
nathanfaucett <nathanfaucett@gmail.com>
2014-12-09 07:48:44 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
+var reEscaper = /\\|'|\r|\n|\t|\u2028|\u2029/g,
+    ESCAPES = {
+        "'": "'",
+        "\\": "\\",
+        "\r": "r",
+        "\n": "n",
+        "\t": "t",
+        "\u2028": "u2028",
+        "\u2029": "u2029"
+    };
+
+
+module.exports = template;
+
+
 function template(text, data, settings) {
-    var render,
-        escapes = template.escapes,
-        escaper = template.escaper,
-        templateSettings = template.settings,
+    var templateSettings = template.settings,
 
-        start, end, match = "([\\s\\S]+?)",
-        evaluate, interpolate, escape,
-
+        match = "([\\s\\S]+?)",
+        source = "__p+='",
         index = 0,
-        source = "__p+='";
+
+        render, start, end, evaluate, interpolate, escape;
 
     settings || (settings = {});
+
     for (var key in templateSettings) {
         if (settings[key] == null) settings[key] = templateSettings[key];
     }
+
     start = settings.start;
     end = settings.end;
 
@@ -24,13 +38,20 @@ function template(text, data, settings) {
     text.replace(
         new RegExp(escape + "|" + interpolate + "|" + evaluate + "|$", "g"),
         function(match, escape, interpolate, evaluate, offset) {
-            source += text.slice(index, offset).replace(escaper, function(match) {
-                return '\\' + escapes[match];
+
+            source += text.slice(index, offset).replace(reEscaper, function(match) {
+                return '\\' + ESCAPES[match];
             });
 
-            if (escape) source += "'+\n((__t=(" + escape + "))==null?'':escape(__t))+\n'";
-            if (interpolate) source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
-            if (evaluate) source += "';\n" + evaluate + "\n__p+='";
+            if (escape) {
+                source += "'+\n((__t=(" + escape + "))==null?'':escape(__t))+\n'";
+            }
+            if (interpolate) {
+                source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+            }
+            if (evaluate) {
+                source += "';\n" + evaluate + "\n__p+='";
+            }
 
             index = offset + match.length;
 
@@ -40,7 +61,7 @@ function template(text, data, settings) {
     source += "';\n";
 
     if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
-    source = "var __t,__p='',__j=Array.prototype.join, print=function(){__p+=__j.call(arguments,'');};\n" + source + "return __p;\n";
+    source = "var __t,__p='',__j=Array.prototype.join;\n" + source + "return __p;\n";
 
     try {
         render = new Function(settings.variable || 'obj', source);
@@ -48,19 +69,11 @@ function template(text, data, settings) {
         e.source = source;
         throw e;
     }
-    if (data) {
-        return render(data);
-    }
 
-    function temp(data) {
+    return data != null ? render(data) : function temp(data) {
         return render.call(this, data);
-    }
-    temp.source = 'function(' + (settings.variable || 'obj') + '){\n' + source + '}';
-
-    return temp;
+    };
 }
-
-template.escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
 
 template.settings = {
     start: "<%",
@@ -68,16 +81,3 @@ template.settings = {
     interpolate: "=",
     escape: "-"
 };
-
-template.escapes = {
-    "'": "'",
-    "\\": "\\",
-    "\r": "r",
-    "\n": "n",
-    "\t": "t",
-    "\u2028": "u2028",
-    "\u2029": "u2029"
-};
-
-
-module.exports = template;
